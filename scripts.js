434118c1db44f283d54e777bcdcc73abc1bb6929
Pablo <pablo.olavarr@gmail.com>
2021-05-28 12:09:17 -0700

    first commit

---
+++ scripts.js
@@ -0,0 +1,18 @@
+$(document).ready(function(){
+    $(".amount-item").on("click", (function(){
+        $(".amount-item").removeClass("amount-item-active");
+        $(this).addClass("amount-item-active");
+        $(".how-much").removeClass("how-much-active");
+        if ($(this).attr('id') == "custom-amount") {
+            $(".how-much").toggleClass("how-much-active");
+        }
+    }))
+
+    $(".memory-honor").on("click", (function(){
+        $(this).toggleClass("memory-honor-active");
+    }))
+
+    $(".toggle").on("click", (function(){
+        $(this).toggleClass("toggle-active");
+    }))
+})
